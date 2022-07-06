eb1cc23041b7fe75c653e4e8b707f7d4f1675ff8
RodrigoBorgognoni <80656730+RodrigoBorgognoni@users.noreply.github.com>
2022-07-06 16:18:44 -0300

    Adicionar arquivos de projeto.

---
+++ TxtToSpeech/TxtToSpeech/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Plugin.TextToSpeech;
+using System;
+using Xamarin.Forms;
+
+namespace TxtToSpeech
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ClickedButton(object Sender, EventArgs args)
+        {
+            var text = Enter.Text;
+
+            CrossTextToSpeech.Current.Speak(text);
+        }
+
+
+    }
+}
